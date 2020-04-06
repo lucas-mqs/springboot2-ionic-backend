adc477865603c73fb51fedf682f479e722bdc3a2
Lucas Marques <lucas.souzamqs@gmail.com>
2020-04-06 15:20:44 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/lucasmarques/cursomc/services/PedidoService.java
@@ -0,0 +1,22 @@
+package com.lucasmarques.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucasmarques.cursomc.domain.Pedido;
+import com.lucasmarques.cursomc.repositories.PedidoRepository;
+import com.lucasmarques.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> opt = repo.findById(id);
+		return opt.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getSimpleName()));
+	}
+}
